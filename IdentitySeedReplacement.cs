7d675493623e9be7707605622acba4c2a4a2ced3
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2021-03-10 11:32:44 -0800

    Update IdentitySeedReplacement.cs
    fixed spelling mistakes

--- IdentitySeedReplacement.cs
+++ IdentitySeedReplacement.cs
@@ -11,7 +11,7 @@
 // The table Id is not currently accessible in the formula engine and has to be hard-coded to match the table in which the field is added.
 // The multiplier in this example of 1000000000 allows up to a 999,999,999 records in a table and for the table Id to increase from the current base value of 10,000 (resulting in a precision 14 value) up to 999,999 (resulting in a precision 15 value) without having to adjust the formula or any subsequent dependencies.
 // The multiplier can be reduced to change the number of zeros padded to the original recordId based on the number of anticipated records the table will eventually hold.
-// In this exmaple the hard-coded table Id is 10013 (system prebuilt Risk Exceptions table)
+// In this example the hard-coded table Id is 10013 (system prebuilt Risk Exceptions table)
 // Assuming a record Id of 5 for this example the result unique record Id would be 10013000000005
 
 decimal tableId = 10013;
