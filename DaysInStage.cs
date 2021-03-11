fe1d5904fc8dacae319bcc56662cc4db4f98ede6
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2021-03-11 11:41:50 -0800

    Create DaysInStage.cs

---
+++ DaysInStage.cs
@@ -0,0 +1,18 @@
+// Recalculation Interval: 24 hours
+// Dependancies: _DateTimeEnteredStage, https://github.com/LockpathUsers/LockpathFormulas/blob/master/DateTimeEnteredWorkflowStage.cs
+
+// Purpose: Calculates the number of days a record has been in the same workflow stage.
+
+// Formula Field Configuration:
+//   Type: Numeric
+//   Alias: Any
+
+// Known Issues / Hard-Coded Values: The formula currrently takes the raw difference in dates and formats the output to days.
+
+// Formula Description: Tracks how many days a record has been in the same workflow stage.
+
+DateTime? enteredWorkflowStageDate = record.GetValue<DateTime?>("_DateTimeEnteredStage");
+DateTime? currentDate =  DateTime.Now;
+return (decimal)((currentDate.Value - enteredWorkflowStageDate.Value).TotalDays);
+
+// Notes: The value returned to the field can be updated to show weeks, months, years, etc.
