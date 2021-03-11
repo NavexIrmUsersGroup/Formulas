896d8e7c75694f711ed8c4e0dd9ce35391aff9b1
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2021-03-11 11:08:31 -0800

    added missing variable declarations

--- DateTimeEnteredWorkflowStage.cs
+++ DateTimeEnteredWorkflowStage.cs
@@ -4,15 +4,18 @@
 // Purpose: There is no system field that captures when a record enters a workflow stage. The Updated field will show when a record enters a stage this value will only be accurate if the record is not edited while remaining in the same workflow stage since editing the record will change the value in the Updated field.
 
 // Formula Field Configuration:
-//  Type: Date
-//  Alias: DateTimeEnteredStage
-//  Include Time: Yes
+//   Type: Date
+//   Alias: DateTimeEnteredStage
+//   Include Time: Yes
 
 // Known Issues / Hard-Coded Values:
 // As this formula has a dependancy that is also a formula the best approach is to set a specific Initial Recalculation date and time that is at least 10 minutes later than the the Initial Recalculation set on the field that this formula is dependant.
 
 // Formula Description:
 // This is a self referencing field that only updates itself if the workflow stage Id changes.
+
+DateTime? dateExistingValue = record.GetValue<DateTime?>("_DateTimeEnteredStage");
+String strWorkflowDateTimeStamp = record.GetValue<String>("_StageIdAndTimeStamp");
 
 if (!string.IsNullOrEmpty(strExistingValue)) { // if the field is not empty get the workflow stage id from the _StageIdAndTimeStamp field value
     int endPosition = strExistingValue.IndexOf("-"); // get the poistion of the first delimiter
