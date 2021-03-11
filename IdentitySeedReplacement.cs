41031df39053f496ba864f0369097878d2eb8825
Robert Klohr <39203582+RobertKlohr@users.noreply.github.com>
2021-03-11 10:24:16 -0800

    Update IdentitySeedReplacement.cs
    Changed format to create a template for future formula examples.

--- IdentitySeedReplacement.cs
+++ IdentitySeedReplacement.cs
@@ -1,18 +1,21 @@
-// While record Ids are unique in a table they are not unique in the platform. To create a platform level unique record Id it is necessary to create a numeric field in a table and populate it with a formula.
+// Recalculation Interval: One Time
 
-// Instructions
-//  Create a numeric field in the table with the following settings:
-//    System Name: IdUnique
+// Purpose: Identiy Seeds can be used to create unique record Ids.  The problem with using idenity seeds is that they base seed is reset if the formula is changed thereby changing the "unique id".
+// This formula can be used to create a platform level unique record Id that never changes.
+
+// Formula Field Configuration:
+//  Type: Numeric
 //    Precision: 15
 //    Scale: 0
 
-// Add the formula below to the field that will hold the unique record Id
-
+// Known Issues / Hard-Coded Values:
 // The table Id is not currently accessible in the formula engine and has to be hard-coded to match the table in which the field is added.
-// The multiplier in this example of 1000000000 allows up to a 999,999,999 records in a table and for the table Id to increase from the current base value of 10,000 (resulting in a precision 14 value) up to 999,999 (resulting in a precision 15 value) without having to adjust the formula or any subsequent dependencies.
-// The multiplier can be reduced to change the number of zeros padded to the original recordId based on the number of anticipated records the table will eventually hold.
-// In this example the hard-coded table Id is 10013 (system prebuilt Risk Exceptions table)
-// Assuming a record Id of 5 for this example the result unique record Id would be 10013000000005
+// The multiplier needs to be a multiple of 10 with the value determining the number of possible records that the table will hold up to a current maximum of 10000000000 based on table Ids being 5 digits and the field limit being 15 places.
+
+// Formula Description:
+// In this example the hard-coded table Id is 10013 (system prebuilt Risk Exceptions table).
+// In this example of 1000000000 allows up to a 999,999,999 records in a table and for the table Id to increase from the current base value of 10,000 or 5 places (resulting in a precision 14 value) up to 999,999 (resulting in a precision 15 value) without having to adjust the formula or any subsequent dependencies.
+// Assuming a record Id of 5 for this example the result unique record Id would be 10013000000005.
 
 decimal tableId = 10013;
 decimal multiplier = 1000000000;
@@ -20,8 +23,8 @@ decimal recordId = record.GetValue<int>("Id");
 
 return tableId * multiplier + recordId;
 
+// Notes:
 // To reverse the unique record Id into it's two components you would use the following formula
-
 // decimal UniqueRecordId = record.GetValue<int>("IdUnique");  // value from the field for this example is 10013000000005
 // decimal recordId = UniqueRecordIdField % 1000000000 // result is 5
 // decimal tableId = (UniqueRecordId - recordId) / 1000000000 // result is 10013
